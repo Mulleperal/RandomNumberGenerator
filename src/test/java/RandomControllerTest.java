c4c609774320e22cffcea14986ece614de5dbe40
wi20b016 <wi20b016@technik-wien.at>
2021-06-30 18:54:12 +0200

    Kommentare in den Unit Tests "RandomControllerTest" bearbeitet

--- src/test/java/RandomControllerTest.java
+++ src/test/java/RandomControllerTest.java
@@ -5,21 +5,15 @@ import org.junit.jupiter.api.Test;
 public class RandomControllerTest {
     @Test
     public void test_generator_Below100(){
-        // Arrange
         RandomController rng = new RandomController();
-        // Act
         int tmp = rng.generator();
-        // Assert
         Assertions.assertEquals(true,tmp <= 100);
     }
 
     @Test
     public void test_generator_Above0(){
-        // Arrange
         RandomController rng = new RandomController();
-        // Act
         int tmp = rng.generator();
-        // Assert
         Assertions.assertEquals(true,tmp >= 0);
     }
 }
